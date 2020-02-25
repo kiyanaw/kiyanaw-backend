b56886e4128fcc1c2e8bcdc089231f466243c512
Aaron Fay <aaron.j.fay@gmail.com>
2020-02-25 14:43:21 -0800

    transpile vuetify deps

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-    .BundleAnalyzerPlugin
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
   runtimeCompiler: true,
   configureWebpack: {
     // turn this on to see where the bulk of the app is
     // plugins: [new BundleAnalyzerPlugin()]
-  }
+  },
+  transpileDependencies: ['vuetify']
 }
