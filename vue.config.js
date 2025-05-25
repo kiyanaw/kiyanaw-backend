18e73e16bc16f7b8c178345c47a74cd47dbbcb4b
Aaron Fay <aaron.j.fay@gmail.com>
2025-05-24 18:13:53 -0700

    builds, but with warnings

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,17 @@ module.exports = {
           use: ['source-map-loader'],
         }
       ]
+    },
+    resolve: {
+      fallback: {
+        "assert": require.resolve("assert/"),
+        "path": require.resolve("path-browserify"),
+        "fs": false,
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "buffer": require.resolve("buffer/"),
+        "util": require.resolve("util/")
+      }
     }
   },
   transpileDependencies: ['vuetify'],
