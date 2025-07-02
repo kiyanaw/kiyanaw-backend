08d4b4348ac5db6d4cdb7faa0386dbef1930bdae
Aaron Fay <aaron.j.fay@gmail.com>
2025-07-02 16:51:25 -0700

    adds newauth env

--- amplify/#current-cloud-backend/function/indexRegionData/src/lib/es.js
+++
@@ -1,8 +0,0 @@
-const esClient = require('elasticsearch').Client({
-  host: 'https://search-indexregiondata-lqatyzsxiuhepcfidwldyiebh4.us-east-1.es.amazonaws.com',
-  connectionClass: require('http-aws-es'),
-})
-
-module.exports = {
-  client: esClient,
-}
