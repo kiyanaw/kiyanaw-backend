9df1045a868b7d21321987257047919f3a1dec53
Aaron Fay <aaron.j.fay@gmail.com>
2025-07-04 09:18:59 -0700

    exclude tests from lint process

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'src_old'] },
+  { ignores: ['dist', 'src_old', '**/*.test.*'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
