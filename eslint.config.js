2e37ae7604cc23e38bb411ccb197d6ebd3ed5306
Dan Fay <dan.c.fay@gmail.com>
2025-07-03 22:31:38 -0600

    Update eslint config based on recommendation in the readme and remove deprecated file

--- eslint.config.js
+++ eslint.config.js
@@ -5,13 +5,17 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist', 'src_old', '**/*.test.*'] },
+  { ignores: ['dist', '**/*.test.*'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
