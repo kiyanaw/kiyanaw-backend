81e799c5b96563bc3dc215938271429672d23010
Aaron Fay <aaron.j.fay@gmail.com>
2025-07-04 20:46:55 -0700

    remove eslint rules; add missing types

--- eslint.config.js
+++ eslint.config.js
@@ -27,8 +27,6 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-      // '@typescript-eslint/no-explicit-any': 'off',
-      // '@typescript-eslint/no-implicit-any': 'off',
     },
   },
 )
