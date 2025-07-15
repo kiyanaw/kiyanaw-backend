7e00297075fff996ea0322f2501db53eff3fd21a
Dan Fay <dan.c.fay@gmail.com>
2025-07-15 17:40:27 -0600

    Ingnore lint error, param was added as part of fixing a CDK build error

--- amplify/backend/custom/regionChangeSQS/cdk-stack.ts
+++ amplify/backend/custom/regionChangeSQS/cdk-stack.ts
@@ -18,6 +18,7 @@ export class cdkStack extends cdk.Stack {
       description: 'Name of the indexRegionData function',
     });
 
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const apikiyanawGraphQLAPIIdOutput = new cdk.CfnParameter(this, 'apikiyanawGraphQLAPIIdOutput', {
       type: 'String',
       description: 'GraphQL API ID for kiyanaw',
