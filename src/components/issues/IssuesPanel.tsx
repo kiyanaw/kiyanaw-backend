ec103bef771fbcfac8db654a149445f7255bccb6
Aaron Fay <aaron.j.fay@gmail.com>
2025-07-15 21:00:14 -0700

    remove useAuth from issues panel : '

--- src/components/issues/IssuesPanel.tsx
+++ src/components/issues/IssuesPanel.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { useAuth } from '../../hooks/useAuth';
+import { useAuthStore } from '../../stores/useAuthStore';
 
 interface Issue {
   id: string;
@@ -51,7 +51,7 @@ export const IssuesPanel = ({
   onDeleteIssue,
   onAddComment,
 }: IssuesPanelProps) => {
-  const { user } = useAuth();
+  const user = useAuthStore((state) => state.user);
   const [showCreateForm, setShowCreateForm] = useState(false);
   const [newIssueText, setNewIssueText] = useState('');
   const [newIssueType, setNewIssueType] = useState<Issue['type']>('general');
