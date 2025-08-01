4227b151b5d868c5d729e45d664fa2093055d2af
Aaron Fay <aaron.j.fay@gmail.com>
2025-08-01 12:00:53 -0700

    consolidate test runs

--- jest.config.js
+++ jest.config.js
@@ -1,42 +1,61 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '^../aws-exports.js$': '<rootDir>/src/__mocks__/aws-exports-mock.js',
-    '^../graphql/queries.js$': '<rootDir>/src/graphql/queries.js',
-    '^../graphql/mutations.js$': '<rootDir>/src/graphql/mutations.js',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(@aws-amplify|aws-amplify)/)',
-  ],
-  transform: {
-    '^.+\\.(t|j)sx?$': ['ts-jest', {
+  projects: [
+    // Main React/TypeScript tests
+    {
+      displayName: 'Main App',
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom',
+      setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '^../aws-exports.js$': '<rootDir>/src/__mocks__/aws-exports-mock.js',
+        '^../graphql/queries.js$': '<rootDir>/src/graphql/queries.js',
+        '^../graphql/mutations.js$': '<rootDir>/src/graphql/mutations.js',
+      },
+      transformIgnorePatterns: [
+        'node_modules/(?!(@aws-amplify|aws-amplify)/)',
+      ],
+      transform: {
+        '^.+\\.(t|j)sx?$': ['ts-jest', {
           tsconfig: {
-      jsx: 'react-jsx',
-      esModuleInterop: true,
-      allowSyntheticDefaultImports: true,
-      allowJs: true,
-      types: ['@testing-library/jest-dom'],
+            jsx: 'react-jsx',
+            esModuleInterop: true,
+            allowSyntheticDefaultImports: true,
+            allowJs: true,
+            types: ['@testing-library/jest-dom'],
+          },
+        }],
+      },
+      testMatch: [
+        '<rootDir>/src/**/*.(test|spec).(ts|tsx)',
+      ],
+      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+      collectCoverageFrom: [
+        'src/**/*.(ts|tsx)',
+        '!src/**/*.d.ts',
+        '!src/**/*.(test|spec).(ts|tsx)',
+      ],
+      testPathIgnorePatterns: [
+        '/node_modules/',
+        '/amplify/',
+      ],
     },
-    }],
-  },
-  testMatch: [
-    '<rootDir>/src/**/*.(test|spec).(ts|tsx)',
-  ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
-  collectCoverageFrom: [
-    'src/**/*.(ts|tsx)',
-    '!src/**/*.d.ts',
-    '!src/**/*.(test|spec).(ts|tsx)',
-  ],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/amplify/backend/(?!function/)',
-    '/amplify/#current-cloud-backend/',
-    '/amplify/cli.json',
-    '/amplify/hooks/',
+    // Lambda function tests
+    {
+      displayName: 'Lambda Functions',
+      testEnvironment: 'node',
+      rootDir: 'amplify/backend/function/inviteHandler/src',
+      testMatch: [
+        '<rootDir>/actions/**/*.(test|spec).(js|ts)',
+      ],
+      collectCoverageFrom: [
+        'actions/**/*.(js|ts)',
+        '!actions/**/*.(test|spec).(js|ts)',
+      ],
+      testPathIgnorePatterns: [
+        '/node_modules/',
+      ],
+    },
   ],
 }; 
