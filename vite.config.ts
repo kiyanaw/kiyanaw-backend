44ee93d3193234fe5b8da326a34a9a9da34fc4e8
Aaron Fay <aaron.j.fay@gmail.com>
2025-08-17 12:17:30 -0700

    disable vite dev config

--- vite.config.ts
+++ vite.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
         clientsClaim: true
       },
       devOptions: {
-        enabled: true
+        enabled: false  // Disable service worker in dev to avoid conflicts
       }
     })
   ],
