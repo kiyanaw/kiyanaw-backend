0a89afd1beb67b8d6583b42fd4e5986ceb823d33
Dan Fay <dan.c.fay@gmail.com>
2026-03-17 08:15:32 -0600

    Fix lint error

--- src/setupTestGlobals.ts
+++ src/setupTestGlobals.ts
@@ -1,2 +1,11 @@
-(globalThis as any).__SPELLCHECK_BASE_URL__ = 'https://api.kiyanaw.dev';
-(globalThis as any).__SPELLCHECK_API_KEY__ = 'test-key';
+export {};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __SPELLCHECK_BASE_URL__: string;
+  // eslint-disable-next-line no-var
+  var __SPELLCHECK_API_KEY__: string;
+}
+
+globalThis.__SPELLCHECK_BASE_URL__ = 'https://api.kiyanaw.dev';
+globalThis.__SPELLCHECK_API_KEY__ = 'test-key';
